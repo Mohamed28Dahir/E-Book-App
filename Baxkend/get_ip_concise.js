dd69200c2fbb137f23fa553fa5e57218252e21a3
Caesar mohamed <zaysarabas@gmail.com>
2026-02-07 13:31:24 +0300

    Update get_ip_concise.js

--- Baxkend/get_ip_concise.js
+++ Baxkend/get_ip_concise.js
@@ -3,7 +3,6 @@ const interfaces = os.networkInterfaces();
 for (const name of Object.keys(interfaces)) {
     for (const iface of interfaces[name]) {
         if ('IPv4' === iface.family && !iface.internal) {
-            console.log(`${iface.address} - ${name}`);
         }
     }
 }
