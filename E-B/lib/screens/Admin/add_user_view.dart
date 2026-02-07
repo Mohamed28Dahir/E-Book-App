ab0145ffdafac5dd3662ac3bfa8159d56e1e78ef
maaidomowliid72-a11y <maaidomowliid72@gmail.com>
2026-02-07 15:06:35 +0300

    Update add_user_view.dart

--- E-B/lib/screens/Admin/add_user_view.dart
+++ E-B/lib/screens/Admin/add_user_view.dart
@@ -4,6 +4,8 @@ import '../utils/app_colors.dart';
 import '../../api/api_service.dart';
 import 'admin_controller.dart';
 
+
+
 class AddUserView extends StatefulWidget {
   final Map<String, dynamic>? userData;
   const AddUserView({super.key, this.userData});
