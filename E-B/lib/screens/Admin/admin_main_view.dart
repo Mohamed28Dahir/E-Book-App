18c4d5fb69007a97e5a439d2bde7c97548aa40eb
Mohamed28Dahir <mohameddahirosman26@gmail.com>
2026-02-07 13:38:48 +0300

    Remove build method from AdminMainView
    Removed the build method implementation in AdminMainView.

--- E-B/lib/screens/Admin/admin_main_view.dart
+++ E-B/lib/screens/Admin/admin_main_view.dart
@@ -9,21 +9,7 @@ import 'profile_view.dart';
 class AdminMainView extends GetView<AdminController> {
   const AdminMainView({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Obx(() {
-      switch (controller.selectedIndex.value) {
-        case 0:
-          return const DashboardView();
-        case 1:
-          return const ManageBooksView();
-        case 2:
-          return const ManageUsersView();
-        case 3:
-          return const ProfileView();
-        default:
-          return const DashboardView();
-      }
+
     });
   }
 }
